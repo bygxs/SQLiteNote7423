07e8dbe68efbf6060551c570d26271dc2d8f9787
bygxs <114972065+bygxs@users.noreply.github.com>
2023-04-07 23:14:02 +0200

    07 April 2023 Friday 23:13 btnSave on AddNoteActivity.kt to create note works

--- app/src/main/java/com/biniyam/sqlitenote7423/AddNoteActivity.kt
+++ app/src/main/java/com/biniyam/sqlitenote7423/AddNoteActivity.kt
@@ -2,10 +2,29 @@ package com.biniyam.sqlitenote7423
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.biniyam.sqlitenote7423.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var noteDao: NoteDao
+    private lateinit var noteList: List<NoteModel>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_note)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.etTitle.text.toString()
+        binding.etNoteDetail.text.toString()
+
+        binding.btnSave.setOnClickListener {
+            val note = NoteModel (
+                -1, binding.etTitle.text.toString(),
+                binding.etNoteDetail.text.toString()
+                    )
+            Toast.makeText(this, note.toString(),Toast.LENGTH_LONG).show()
+        }
     }
 }
